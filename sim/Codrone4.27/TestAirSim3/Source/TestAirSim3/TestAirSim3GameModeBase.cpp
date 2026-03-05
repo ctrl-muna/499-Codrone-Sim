708a4c804657c8b7632f65034d55bf9a035811da
sopuru-ani <sopuruani41@gmail.com>
2026-03-04 22:59:10 -0500

    4.27 failure that works

---
+++ sim/Codrone4.27/TestAirSim3/Source/TestAirSim3/TestAirSim3GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "TestAirSim3GameModeBase.h"
+
---
+++ sim/Codrone4.27/TestAirSim3/Source/TestAirSim3/TestAirSim3GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestAirSim3GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTAIRSIM3_API ATestAirSim3GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
