56b4f0a807814e4d86bba95ed557691707945cc0
Burton <ncburton@umes.edu>
2026-03-11 13:47:01 -0400

    Addedpython script control for the sdk for several commands

---
+++ sdk/client/DroneClientControl.py
@@ -0,0 +1,41 @@
+##Imports for airsim
+import asyncio
+
+from projectairsim import ProjectAirSimClient, Drone, World
+from projectairsim.utils import projectairsim_log
+from projectairsim.image_utils import ImageDisplay
+
+
+###imports for user Control
+import time
+import csv
+import os
+import re
+import json
+import asyncio
+from UserControl import UserControl
+
+async def main():
+    DroneController = UserControl()
+    DroneController.connect()
+    Running  = True
+    ###Intro
+    try:
+        print("Welcome to to Class 499's Codrone Simulator!\n We will begin the connecting process now.")
+        while Running:
+            await DroneController.Input_Command()
+            Input = input("Continue Flying? (y/n): ")
+            if Input.lower() == "n":
+                Running = False
+    except KeyboardInterrupt:
+        print("\nInterrupted by user.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        print("Exiting Drone Control. Goodbye!\n")
+        DroneController.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
